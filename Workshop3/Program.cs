41dc77f014d8724d6a5d8abdc7daccc4ca826f41
shres <shres@GAURAV_1>
2025-11-12 09:51:11 +0545

    Task 3 completed

--- Workshop3/Program.cs
+++ Workshop3/Program.cs
@@ -17,5 +17,27 @@ class Program
         NullOperations nullOps = new NullOperations();
         nullOps.PerformNullChecks();
 
+        //Task3
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age < 13)
+        {
+            Console.WriteLine("You are a child.");
+        }
+        else if (age >= 13 && age < 20)
+        {
+            Console.WriteLine("You are a teenager.");
+        }
+        else if (age >= 20 && age < 60)
+        {
+            Console.WriteLine("You are an adult.");
+        }
+        else
+        {
+            Console.WriteLine("You are a senior citizen.");
+        }
+
+        
     }
 }
