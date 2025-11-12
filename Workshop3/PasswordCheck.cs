e5b6e8bb68412cce200ef4f9f90e0f3501e14f82
shres <shres@GAURAV_1>
2025-11-12 20:45:53 +0545

    Password class created

---
+++ Workshop3/PasswordCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class PasswordCheck
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your password: ");
+        string password = Console.ReadLine();
+
+        try
+        {
+            if (password.Length < 6)
+            {
+                throw new Exception("Password must be at least 6 characters.");
+            }
+            Console.WriteLine("Password satisfied the requirement");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.ReadLine();
+    }
+}
